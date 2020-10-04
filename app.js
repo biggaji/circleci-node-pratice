b86eedb02aebf7074ad28071ae21c4d32dde6509
Tobi Ajibade <herityjohnny14>
2020-10-04 16:26:53 -0700

    Added index router to controller

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // bring in express
 const express = require("express");
+const indexRouter = require('./routes/index_Routes');
 const session = require("express-session");
 const cookieParser = require('cookie-parser');
 
 const app = express();
 app.use(cookieParser());
 app.use(session({ name: "sid", secret: "Baby", resave: false, saveUninitialized: false }));
-
-
+app.use('/', indexRouter);
 app.listen(8000, () => {
     console.log('Server running on port 8000 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 })
--- routes/index_Routes.js
+++ routes/index_Routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 
-const { index } = require('./controllers/index_Controller.js');
+const { index } = require('../controllers/index_Controller.js');
 const router = express.Router();
 
 
 router.get("/", index);
+
+module.exports = router;
